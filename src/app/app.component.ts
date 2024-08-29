3ae14d4ad528d61a784d9632a5c329ca5f66d19e
Mahor Pradeepkumar (Mumbai - IT) <p.mahor@tecnimont.in>
2024-08-29 16:05:49 +0530

    Header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layput/header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'Pradeep Technology';
+
+  baseUrl = 'https://localhost:7002/'
+  private http = inject(HttpClient)
+
+  ngOnInit(): void {
+
+    this.http.get(this.baseUrl + 'Products').subscribe({
+      next: data => alert(data),
+      error: error => alert(error),
+      complete: () => console.log('complete')
+    })
+
+  }
+
+
 }
